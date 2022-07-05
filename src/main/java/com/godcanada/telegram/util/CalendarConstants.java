5fe47d1415473d766d3b56de4f4a93f85d9323b9
Sujatha Raghuraman <testid216@gmail.com>
2022-07-05 13:23:20 +0530

    Initial commit

---
+++ src/main/java/com/godcanada/telegram/util/CalendarConstants.java
@@ -0,0 +1,9 @@
+package com.godcanada.telegram.util;
+
+public class CalendarConstants {
+    public static final String TELEGRAM_SEND_PHOTO = "sendPhoto";
+    public static final String TELEGRAM_SEND_MESSAGE = "sendMessage";
+    public static final String LAST_SLOT = "Last Slot";
+    public static final String AMERICA_TORONTO = "America/Toronto";
+    public static final String START_TIME = "startTime";
+}
