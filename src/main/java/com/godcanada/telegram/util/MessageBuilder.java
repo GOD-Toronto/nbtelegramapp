21f4d23098c96e91d4c64a1b38e352d254bb6c1f
Sujatha Raghuraman <testid216@gmail.com>
2022-07-05 13:23:20 +0530

    Initial commit

---
+++ src/main/java/com/godcanada/telegram/util/MessageBuilder.java
@@ -0,0 +1,18 @@
+package com.godcanada.telegram.util;
+
+public class MessageBuilder {
+    public static final String newLine = System.getProperty("line.separator");
+
+    public static String removeHTMLBlob(String msgTxt) {
+        return msgTxt
+                .replace("<html-blob>", "")
+                .replace("</html-blob>", "")
+                .replace("<u>", "")
+                .replace("</u>", "")
+                .replace("&nbsp;", " ")
+                .replace("<p>", "")
+                .replace("</p>", "")
+                .replace("<br>", newLine);
+    }
+
+}
