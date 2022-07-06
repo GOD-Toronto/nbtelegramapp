1aa5071cd2c2601711e1f4df3f967e25286b1103
Sujatha Raghuraman <testid216@gmail.com>
2022-07-06 15:56:41 +0530

    added volunteer tasks scheduler

--- src/main/java/com/namabhiksha/telegram/schedulers/VolunteerTasksScheduler.java
+++ src/main/java/com/namabhiksha/telegram/schedulers/VolunteerTasksScheduler.java
@@ -12,31 +12,30 @@ import java.util.concurrent.TimeUnit;
 
 import static com.namabhiksha.telegram.util.CalendarConstants.AMERICA_TORONTO;
 
-public class NamaSlotsScheduler {
+public class VolunteerTasksScheduler {
     private final Calendar calendar;
     private final Drive drive;
     private final CommonUtil commonUtil;
     private final Set<String> parsedTimeSlots;
-    @Value("${util.nama-slots.calendar-id}")
+    @Value("${util.volunteers.calendar-id}")
     private String calendarIdValue;
-    @Value("${util.nama-slots.chat-id}")
+    @Value("${util.volunteers.chat-id}")
     private String chatId;
-    @Value("${util.nama-slots.max-check-time}")
+    @Value("${util.volunteers.max-check-time}")
     private long maxTimeCheck;
 
 
-
     private static final Logger log
-            = org.apache.logging.log4j.LogManager.getLogger(NamaSlotsScheduler.class);
+            = org.apache.logging.log4j.LogManager.getLogger(VolunteerTasksScheduler.class);
 
-    public NamaSlotsScheduler(Calendar calendar, Drive drive, CommonUtil commonUtil) {
+    public VolunteerTasksScheduler(Calendar calendar, Drive drive, CommonUtil commonUtil) {
         this.calendar = calendar;
         this.drive = drive;
         this.commonUtil = commonUtil;
         this.parsedTimeSlots = new HashSet<>();
     }
 
-    @Scheduled(fixedRateString = "${util.nama-slots.schedule-time}", timeUnit = TimeUnit.MINUTES, zone = AMERICA_TORONTO)
+    @Scheduled(fixedRateString = "${util.volunteers.schedule-time}", timeUnit = TimeUnit.MINUTES, zone = AMERICA_TORONTO)
     public void run() throws Exception {
         log.info("run::run invoked");
         commonUtil.getEvents(maxTimeCheck, calendar, calendarIdValue,
