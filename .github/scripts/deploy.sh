cc1303186a67c4172653245981dcaa72f05babe5
babiraman <babiraman@architech.ca>
2022-07-08 16:24:59 -0400

    self runner changes

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -18,7 +18,7 @@ echo "Bringing the App up"
 
 pwd
 
-nohup java -jar nbtelegramapp-0.0.1-SNAPSHOT.jar --spring.profiles.active=seva &
+sudo nohup java -jar nbtelegramapp-0.0.1-SNAPSHOT.jar --spring.profiles.active=seva &
 
 sleep 10
 javapid=$(pidof java)
