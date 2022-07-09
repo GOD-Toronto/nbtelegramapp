dbd9d8e56397758236c7d1afdaaf114730c97eeb
Sujatha Raghuraman <testid216@gmail.com>
2022-07-09 16:02:57 +0530

    removing unwanted files in the build process

--- src/main/java/com/namabhiksha/telegram/config/GoogleUtiliesConfig.java
+++ src/main/java/com/namabhiksha/telegram/config/GoogleUtiliesConfig.java
@@ -19,7 +19,6 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
