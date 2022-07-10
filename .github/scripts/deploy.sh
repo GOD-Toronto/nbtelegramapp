76cd9b72d9f1c26dd40154d7c27111914815f13d
babiraman <babiraman@architech.ca>
2022-07-10 12:12:43 -0400

    github action changes

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -18,12 +18,14 @@ rm -v *.out
 
 echo "Bringing the App up"
 
-pwd
-
 cd /home/ec2-user/namabhiksha
 # nohup java -jar nbtelegramapp-0.0.1-SNAPSHOT.jar --spring.profiles.active=seva &
 
-sudo nohup java -jar nbtelegramapp-0.0.1-SNAPSHOT.jar --spring.profiles.active=seva &
+# sudo nohup java -jar nbtelegramapp-0.0.1-SNAPSHOT.jar --spring.profiles.active=seva &
+DateTimeStamp=$(date +%Y-%m-%d_%H-%M-%S)
+logFileName=log+$DateTimeStamp+.log
+sudo nohup java -jar nbtelegramapp-0.0.1-SNAPSHOT.jar --spring.profiles.active=seva > $logFileName 2>&1 &
+
 
 sleep 10
 javapid=$(pidof java)
