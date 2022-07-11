c1d1eced26d3446c413354f0492afd290f02562c
Sujatha Raghuraman <testid216@gmail.com>
2022-07-11 06:49:25 +0530

    drive integration

--- src/main/java/com/namabhiksha/telegram/util/CalendarConstants.java
+++ src/main/java/com/namabhiksha/telegram/util/CalendarConstants.java
@@ -11,4 +11,9 @@ public class CalendarConstants {
     public static final String IMAGES = "\\images\\";
     public static final String JPG = ".jpg";
     public static final String CONFIG = "\\config\\";
+    public static final String IMAGE_JPEG = "image/jpeg";
+    public static final String AUDIO_MPEG = "audio/mpeg";
+    public static final String AUDIO_X_M_4_A = "audio/x-m4a";
+    public static final String AUDIO_OGG = "audio/ogg";
+    public static final String NAMA_TUNES = "Nama Tune";
 }
